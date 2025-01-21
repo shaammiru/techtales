195e6853542cf41e28b5cd951f950602e2ab0762
shaammiru <msyamil.hamami@gmail.com>
2025-01-21 14:30:16 +0700

    add adsense code snippet to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4527332416095185"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
